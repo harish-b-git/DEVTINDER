12cd1a851f731abbe0be448cde8d34fe2bfd1238
harish-b-git <battaharish15@gmail.com>
2025-09-13 13:12:53 +0530

    routes and handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@ const express = require('express')
 
 const app = express()
 
-app.use('/user',(req,res)=>{
 
-    res.send('Hello Harish')
+app.post('/abc',(req,res)=>{
+console.log("req",req.query);
+    res.send({firstName:'Harish',lastName:'Batta'})
 })
+
+app.get('/user',(req,res)=>{
+
+    res.send('User Data')
+})
+
 app.listen(2323,()=>{
     console.log("Server has started");
 })
