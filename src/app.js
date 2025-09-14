ae55e03ec0fe20d5bdc106bed65bf5129f0c2558
harish-b-git <battaharish15@gmail.com>
2025-09-14 10:42:33 +0530

    route handler and next

--- src/app.js
+++ src/app.js
@@ -1,18 +1,40 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
+app.post("/abc", (req, res) => {
+  console.log("req", req.query);
+  res.send({ firstName: "Harish", lastName: "Batta" });
+});
 
-app.post('/abc',(req,res)=>{
-console.log("req",req.query);
-    res.send({firstName:'Harish',lastName:'Batta'})
-})
+app.get(
+  "/user",
+  (req, res,next) => {
+    console.log("res 1");
+    // res.send("Response 1!!");
+    next()
+  },
+  (req, res,next) => {
+    console.log("res 2");
+    next()
+  },
+  (req, res,next) => {
+    console.log("res 3");
+    // res.send("Respons3 3!!");
+    next()
+  },
+  [(req, res,next) => {
+    console.log("res 4");
+    // res.send("Respons3 3!!");
+    next()
+  },
+  (req, res,next) => {
+    console.log("res 5");
+    res.send("Respons5 !!");
+    // next()
+  }]
+);
 
-app.get('/user',(req,res)=>{
-
-    res.send('User Data')
-})
-
-app.listen(2323,()=>{
-    console.log("Server has started");
-})
+app.listen(2323, () => {
+  console.log("Server has started");
+});
