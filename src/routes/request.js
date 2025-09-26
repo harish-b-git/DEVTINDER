6f38650dec498899ba829fe9cc2603f07953f8de
harish-b-git <battaharish15@gmail.com>
2025-09-26 12:48:54 +0530

    request send apis

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,14 +1,55 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+const requestRouter = express.Router();
 
-const requestRouter =express.Router()
-
-
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  // Sending a connection request
-  console.log("Sending a connection request");
-
-  res.send(user.firstName + "sent the connect request!");
-});
-module.exports= requestRouter
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    const fromUserId = req.user._id;
+    const status = req.params.status;
+    const toUserId = req.params.toUserId;
+    try {
+      const allowedStatus = ["interested", "ignored"];
+      if (!allowedStatus.includes(status)) {
+        throw new Error("Not a valid allowed type");
+      }
+      const toUser = await User.findById(toUserId);
+      console.log(toUser);
+      if (!toUser) {
+        throw new Error("Check your touserId User not found");
+      }
+      if(fromUserId.equals(toUserId)){            /// fromUserId,toUserId are objects so we are checking like this
+         throw new Error('Cannot send connection request to self')
+      }
+      const isConnectionFound = await ConnectionRequest.findOne({
+        $or: [
+          {
+            fromUserId,
+            toUserId,
+          },
+          {
+            fromUserId: toUserId,
+            toUserId: fromUserId,
+          },
+        ],
+      });
+      if (isConnectionFound) {
+        throw new Error("Connection request ALready sent");
+      }
+      //   Creating a new instance of the connectionrequest model
+      const connection = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      await connection.save();
+      res.send("Connection Request Sent from" + req.user.firstName);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  }
+);
+module.exports = requestRouter;
